b7eca8577bff928533895d0986ea47ad3a03804d
Rails Test <soup1234567@gmail.com>
2017-05-31 05:44:47 +0000

    Add, edit, update, show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,47 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+  
   def new 
     @article = Article.new
   end
   
   def create
+    
     #render plain: params[:article].inspect
-    @artice = Article.new(artice_parmas)
-    @article.save
-    redirect_to article_show(@article)
+    @article = Article.new(article_parmas)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+    
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_parmas)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
   
   private
-  def article_params
-    params.require(:article).permit(:title, :descrption)
-  end
+    def article_parmas
+      params.require(:article).permit(:title, :description)
+    end
   
 end
